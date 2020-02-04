97bb812dd1d415495dca628a63a7d60ef3ece111
gaizkazaqhariou <gaizkapolinema@gmail.com>
2020-02-04 16:57:39 +0700

    refreshing commit 3

--- app/src/main/java/com/example/cobarecycleview/MainActivity.java
+++ app/src/main/java/com/example/cobarecycleview/MainActivity.java
@@ -26,8 +26,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         RecyclerView recyclerView = findViewById(R.id.recyclerView);
         LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
         mAdapter = new HotelAdapter(mList);
         recyclerView.setAdapter(mAdapter);
         fillData();
